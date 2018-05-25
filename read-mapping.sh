3c50ff2053d7e84ac6514b5da4887a8137e0f234
Mohammad <smh.oloomi@gmail.com>
2018-05-25 11:24:25 +1000

    Single-end mapping mode.

--- read-mapping.sh
+++ read-mapping.sh
@@ -16,12 +16,20 @@ file_prefix="bowtie"
 bowtie2-build ${ref_genome} ${out_dir}genome-index
 
 # Single mapping, best-match
-bowtie2 -x ${out_dir}genome-index -1 ${reads_out_dir}${reads_file_prefix}_1.fq -2 ${reads_out_dir}${reads_file_prefix}_2.fq \
+bowtie2 -x ${out_dir}genome-index -U ${reads_out_dir}${reads_file_prefix}_1.fq,${reads_out_dir}${reads_file_prefix}_2.fq \
 -S ${out_dir}${file_prefix}-mapping-best-match.sam 2> ${out_dir}${file_prefix}-mapping-best-match-log.txt
 
 # Single mapping, report-all
-bowtie2 -a -x ${out_dir}genome-index -1 ${reads_out_dir}${reads_file_prefix}_1.fq -2 ${reads_out_dir}${reads_file_prefix}_2.fq \
+bowtie2 -a -x ${out_dir}genome-index -U ${reads_out_dir}${reads_file_prefix}_1.fq,${reads_out_dir}${reads_file_prefix}_2.fq \
 -S ${out_dir}${file_prefix}-mapping-report-all.sam 2> ${out_dir}${file_prefix}-mapping-report-all-log.txt
+
+# Paired mapping, best-match
+#bowtie2 -x ${out_dir}genome-index -1 ${reads_out_dir}${reads_file_prefix}_1.fq -2 ${reads_out_dir}${reads_file_prefix}_2.fq \
+#-S ${out_dir}${file_prefix}-mapping-best-match.sam 2> ${out_dir}${file_prefix}-mapping-best-match-log.txt
+
+# Paired mapping, report-all
+#bowtie2 -a -x ${out_dir}genome-index -1 ${reads_out_dir}${reads_file_prefix}_1.fq -2 ${reads_out_dir}${reads_file_prefix}_2.fq \
+#-S ${out_dir}${file_prefix}-mapping-report-all.sam 2> ${out_dir}${file_prefix}-mapping-report-all-log.txt
 
 echo "\n=== Bowtie2 read mapping completed! ===\n"
 
